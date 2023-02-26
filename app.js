629a99cbfe1ae29aa4d80e01edba6ce3cee021e9
lokeshkhade@gmail.com <lokeshkhade@gmail.com>
2023-02-26 23:46:31 +0530

    3- Callbacks

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 console.log('Before');
 
-const user = getUser(1);
-console.log(user);
+getUser(1, function (user) {
+    console.log('User', user);
+    getRepositories(user.gitHubUsername, function (repos) {
+        console.log('Repos', repos);
+    })
+});
+
 console.log('After');
 
-//Callbacks
-//Promises
-//Async/await
 
-function getUser(id) {
+//We add another parameter callback to getUserid.A callback is a function that we are 
+//going to be called when the result of asynchrous operation is ready
+function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a user from a database');
-        return { id: id, Username: 'POP' }
+        callback({ id: id, gitHubUsername: 'POP' })
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling Github API');
+        callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
 }
